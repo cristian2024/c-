7e0f0f83e2a19970fd132d6b231b6ce5d4abc0b1
cristian2024 <61856006+cristian2024@users.noreply.github.com>
2020-06-28 14:51:45 -0500

    Version final en c++ del algoritmo de euclides implementado con arreglos

--- algo_euclides.cpp
+++ algo_euclides.cpp
@@ -2,29 +2,43 @@
 
 using namespace std;
 
-int mcd_e(int a, int b){
+int * new_arreglo(int size){
+    return new int[size];
+}
+
+int * mcd_e(int a, int b){
     
+    //primero calculamos cual es el mayor y cual es el menor
     int a_1=(a>b)?a:b;
     int b_1=(a<b)?a:b;
     
+    //este sera el arreglo que sera retornado, solo es una variable auxiliar
+    /**
+     *1era posicion=Maximo comun divisor 
+     * 2da posicion= el valor del x correspondiente a la funcion (x*a_1+y*b_1)=MCD(a_1,b_1)
+     * 3ra posicion= el valor del y correspondiente a la funcion (x*a_1+y*b_1)=MCD(a_1,b_1)
+     */
+    int * first=new_arreglo(3);
+    
+    //los primeros datos que se calculan son siempre los mismos
     int pri[]={a_1,0,1,0};
     int seg[]={b_1,(int)(a_1/b_1),0,1};
     
-    int aux[4];
-    
+    //La variable val permite saber si tenemos que cargar los datos en el arreglo pri o en el arreglo seg
     bool val=1;
     while(pri[0]!=0&&seg[0]!=0){
         
         if(val){
-           
             pri[0]=pri[0]%seg[0];
             pri[2]=pri[2]-(seg[1]*seg[2]);
             pri[3]=pri[3]-(seg[1]*seg[3]);
             
+            //la posicion 2 del arreglo se carga con una division, pero como siempre llegara el punto de una division por 0, es preferible revisar antes de hacer la operacion
             if(pri[0]!=0&&seg[0]!=0){
                 pri[1]=(int)(seg[0]/pri[0]);
             }
             
+            //se modifica el booleano para que entre al otro arreglo
             val=0; 
         }else{
             
@@ -32,11 +46,12 @@ int mcd_e(int a, int b){
             seg[2]=seg[2]-(pri[1]*pri[2]);
             seg[3]=seg[3]-(pri[1]*pri[3]);
             
+            //la posicion 2 del arreglo se carga con una division, pero como siempre llegara el punto de una division por 0, es preferible revisar antes de hacer la operacion
             if(seg[0]!=0&&pri[0]!=0){
-                
                 seg[1]=(int)(pri[0]/seg[0]);
             }
             
+            //se modifica el booleano para que entre al otro arreglo
             val=1;
         }
         
@@ -45,18 +60,25 @@ int mcd_e(int a, int b){
     
    
     
-    
+    //con estas comparaciones se pretende definir cual fue el ultimo arreglo al que se le editaron los datos, esto con la idea de devolver los valores exactos
     if(val){
-        cout<<pri[0]<<"   x:"<<pri[2]<<"("<<a_1<<")"<<"   y:"<<pri[3]<<"("<<b_1<<")";
+        first[0]=pri[0];
+        first[1]=pri[2];
+        first[2]=pri[3];
+        
     }else{
-        cout<<seg[0]<<"   x:"<<seg[2]<<"("<<a_1<<")"<<"   y:"<<seg[3]<<"("<<b_1<<")";
+        first[0]=seg[0];
+        first[1]=seg[2];
+        first[2]=seg[3];
     }
         
-    return 1;
+    return first;
         
         
     
 }
 
 int main(){
+    int *r=mcd_e(55,300);
+    cout<<r[1];
 }
